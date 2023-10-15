2859b8c07dbd0f199b840312e35da34106735f97
rangioh <145316438+rangioh@users.noreply.github.com>
2023-10-15 16:12:01 +0900

    asyncio 비동기 프로그램 무한루프
    실습예제 완성

--- ansync/event_loop.py
+++ ansync/event_loop.py
@@ -6,7 +6,13 @@ async def say(what, when):
 
 async def stop_after(loop, when):
     await asyncio.sleep(when)
-    print(what)
+    loop.stop()
 
 loop = asyncio.get_event_loop()
-loop.create_task(say('first'))
+loop.create_task(say('first hello', 2))
+loop.create_task(say('second hello', 1))
+loop.create_task(say('third hello', 4))
+loop.create_task(say(loop, 3))
+
+loop.run_forever()
+loop.close()
